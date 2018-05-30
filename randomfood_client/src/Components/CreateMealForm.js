261a3b02bc5eeae937669bfc58e8c839866f405f
Duc Nguyen <ducnguyen@Ducs-MBP.home>
2018-05-30 18:50:05 +0300

    Revert "Update Create Ingredient sub compoenent"
    This reverts commit bcd8422ec784c015c05fa789343db09b03786d41.

--- randomfood_client/src/Components/CreateMealForm.js
+++ randomfood_client/src/Components/CreateMealForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import MealIngredientForm from './Widget/Form/CreateMealIngredient';
-import shortId from 'shortid';
 
 class CreateMealForm extends Component {
     constructor(props) {
@@ -9,47 +8,23 @@ class CreateMealForm extends Component {
         this.state =
             {
                 isCreating: false,
-                IngredientList: [],
-                newMeal: {}
+                IngredientList: []
             }
         this.addIngredient = this.addIngredient.bind(this);
-        this.createRecipe = this.createRecipe.bind(this);
     }
     
-    addIngredient(e)
-    {
-        e.preventDefault();
-        this.setState((prevState) => {
-            return {
-                IngredientList: [...prevState.IngredientList, { clientId: shortId.generate()}]
-            }
-        });
-    }
-
-    createRecipe(e)
-    {
-        e.preventDefault();
-        console.log(this.state.IngredientList);
-    }
-
-    onIngredientUpgrade(ingredient)
+    addIngredient()
     {
         this.setState((prevState) => {
             return {
-                IngredientList: prevState.IngredientList.map(x => x.clientId == ingredient.client)
-                                .then(ingredient => {console.log("pla pla")})
+                IngredientList: [...prevState.IngredientList, {name:"test" }]
             }
         });
     }
 
     render() {
-        let ingredientDOM = this.state.IngredientList.map(i => {return <div>
-                                        <MealIngredientForm onIngredientUpdate = {this.onIngredientUpgrade} 
-                                        Ingredient = {i} key={i.clientId}>
-                                        </MealIngredientForm>
-                                        <i className="far fa-trash-alt"></i> 
-                                    </div> });
-
+        let ingredientDOM = this.state.IngredientList.map(x => {return <MealIngredientForm></MealIngredientForm>});
+        console.log(ingredientDOM);
         return (
             <div className="create-meal-form">
                 <form className="">
@@ -57,19 +32,17 @@ class CreateMealForm extends Component {
                         <div class="form-group row">
                         <label for="example-text-input" class="col-3 col-form-label">Name</label>
                         <div class="col-9">
-                            <input value= {this.state.newMeal.Name} class="form-control" 
-                            type="text" id="example-text-input" />
+                            <input class="form-control" type="text"  id="example-text-input" />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="example-text-input" class="col-3 col-form-label">Cooking Duration</label>
                         <div class="col-2">
-                            <input class="form-control" value = {this.state.newMeal.CookingDuration} 
-                            type="text" type="number" />
+                            <input class="form-control" type="text" type="number" id="example-text-input" />
                         </div>
                         <label for="example-text-input" class="col-2 col-form-label">Picture URL</label>
                         <div class="col-5">
-                            <input class="form-control" value={this.state.newMeal.PictureURL}  type="text"/>
+                            <input class="form-control" type="text"  id="example-text-input" />
                         </div>
                     </div>
                     <hr/>
@@ -78,7 +51,6 @@ class CreateMealForm extends Component {
                         <button onClick={this.addIngredient}>Add</button>
                         {ingredientDOM}
                     </div>
-                    <button className="btn btn-default" onClick={this.createRecipe}>Create Recipe</button>
                 </form>
             </div>
         );
