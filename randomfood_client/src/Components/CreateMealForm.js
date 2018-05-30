90335305effeef575deb9db9231da983abfe3572
Duc Nguyen <>
2018-05-30 23:16:38 +0300

    Add new library to project "shortid"

--- randomfood_client/src/Components/CreateMealForm.js
+++ randomfood_client/src/Components/CreateMealForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import MealIngredientForm from './Widget/Form/CreateMealIngredient';
+import shortId from 'shortid';
 
 class CreateMealForm extends Component {
     constructor(props) {
@@ -8,23 +9,47 @@ class CreateMealForm extends Component {
         this.state =
             {
                 isCreating: false,
-                IngredientList: []
+                IngredientList: [],
+                newMeal: {}
             }
         this.addIngredient = this.addIngredient.bind(this);
+        this.createRecipe = this.createRecipe.bind(this);
     }
     
-    addIngredient()
+    addIngredient(e)
+    {
+        e.preventDefault();
+        this.setState((prevState) => {
+            return {
+                IngredientList: [...prevState.IngredientList, { clientId: shortId.generate()}]
+            }
+        });
+    }
+
+    createRecipe(e)
+    {
+        e.preventDefault();
+        console.log(this.state.IngredientList);
+    }
+
+    onIngredientUpgrade(ingredient)
     {
         this.setState((prevState) => {
             return {
-                IngredientList: [...prevState.IngredientList, {name:"test" }]
+                IngredientList: prevState.IngredientList.map(x => x.clientId == ingredient.client)
+                                .then(ingredient => {console.log("pla pla")})
             }
         });
     }
 
     render() {
-        let ingredientDOM = this.state.IngredientList.map(x => {return <MealIngredientForm></MealIngredientForm>});
-        console.log(ingredientDOM);
+        let ingredientDOM = this.state.IngredientList.map(i => {return <div>
+                                        <MealIngredientForm onIngredientUpdate = {this.onIngredientUpgrade} 
+                                        Ingredient = {i} key={i.clientId}>
+                                        </MealIngredientForm>
+                                        <i className="far fa-trash-alt"></i> 
+                                    </div> });
+
         return (
             <div className="create-meal-form">
                 <form className="">
@@ -32,17 +57,19 @@ class CreateMealForm extends Component {
                         <div class="form-group row">
                         <label for="example-text-input" class="col-3 col-form-label">Name</label>
                         <div class="col-9">
-                            <input class="form-control" type="text"  id="example-text-input" />
+                            <input value= {this.state.newMeal.Name} class="form-control" 
+                            type="text" id="example-text-input" />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="example-text-input" class="col-3 col-form-label">Cooking Duration</label>
                         <div class="col-2">
-                            <input class="form-control" type="text" type="number" id="example-text-input" />
+                            <input class="form-control" value = {this.state.newMeal.CookingDuration} 
+                            type="text" type="number" />
                         </div>
                         <label for="example-text-input" class="col-2 col-form-label">Picture URL</label>
                         <div class="col-5">
-                            <input class="form-control" type="text"  id="example-text-input" />
+                            <input class="form-control" value={this.state.newMeal.PictureURL}  type="text"/>
                         </div>
                     </div>
                     <hr/>
@@ -51,6 +78,7 @@ class CreateMealForm extends Component {
                         <button onClick={this.addIngredient}>Add</button>
                         {ingredientDOM}
                     </div>
+                    <button className="btn btn-default" onClick={this.createRecipe}>Create Recipe</button>
                 </form>
             </div>
         );
--- randomfood_client/src/Components/Widget/Form/CreateMealIngredient.js
+++ randomfood_client/src/Components/Widget/Form/CreateMealIngredient.js
@@ -5,19 +5,50 @@ class MealIngredientForm extends Component
     constructor(props)
     {
         super(props);
+        this.state = {
+            Ingredient : {},
+            test: ""
+        }
+        this.onNameChange = this.onNameChange.bind(this);
+        this.Test = this.Test.bind(this);
     }
+
+    componentDidMount()
+    {
+        this.setState({Ingredient: this.props.Ingredient});
+    }
+
+    onNameChange(e)
+    {  const nameValue = e.target.value;
+       this.setState((prevState) => {
+           return { Ingredient: { ...prevState.Ingredient, Name: nameValue}  }
+       })
+    }
+
+    onAmountChange(e)
+    {
+
+    }
+
+    Test(e)
+    {
+        e.preventDefault();
+        console.log(this.state.Ingredient);
+    }
+
     render()
     {   
         return(
-            <div class="form-group row" style = {style}>
+            <div class="form-group row">
                 <label for="example-text-input" class="col-1 col-form-label">Name</label>
                 <div class="col-3">
-                    <input class="form-control" type="text" id="example-text-input" />
+                    <input class="form-control" onChange={this.onNameChange} value={this.state.Ingredient.Name} type="text" id="example-text-input" />
                 </div>
                 <label for="example-text-input" class="col-1 col-form-label">Amount</label>
                 <div class="col-2">
-                    <input class="form-control" type="text" id="example-text-input" />
+                    <input class="form-control" onChange={this.onDataChange} value={this.state.Ingredient.Amount} type="text" id="example-text-input" />
                 </div>
+                <button onClick={this.Test}>Test</button>
             </div>
         );
     }
