03652f0dfa2e0ec3a107310e42c3274dde76179e
Duc Nguyen <nnduc1994@gmail.com>
2018-05-31 22:52:29 +0300

    Update add ingredient by using props instead of state

--- randomfood_client/src/Components/Widget/Form/CreateMealIngredient.js
+++ randomfood_client/src/Components/Widget/Form/CreateMealIngredient.js
@@ -9,9 +9,7 @@ const INPUT_TYPE =
 class MealIngredientForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            Ingredient: this.props.Ingredient
-        }
+
         this.onDataChange = this.onDataChange.bind(this);
         this.inputOnBlur = this.inputOnBlur.bind(this);
         this.delete = this.delete.bind(this);
@@ -19,28 +17,27 @@ class MealIngredientForm extends Component {
 
     inputOnBlur(e)
     {
-        console.log(this.state.Ingredient)
-        this.props.UpdateIngredient(this.state.Ingredient);
+        this.props.UpdateIngredient(this.props.Ingredient);
     }
 
 
     delete()
     {
-        this.props.DeleteIngredient(this.state.Ingredient.clientId);
+        this.props.DeleteIngredient(this.props.Ingredient.clientId);
     }
 
     onDataChange(e) {
         const inputValue = e.target.value;
         const inputId = e.target.id;
-        console.log(inputValue)
-        this.setState((prevState) => {
-            switch (inputId) {
-                case INPUT_TYPE.NAME:
-                    return { Ingredient: { ...prevState.Ingredient, Name: inputValue}  }
-                case INPUT_TYPE.AMOUNT:
-                    return { Ingredient: { ...prevState.Ingredient, Amount: inputValue}  }
-            }
-        });
+    
+        switch (inputId) {
+            case INPUT_TYPE.NAME:
+                this.props.Ingredient.Name = inputValue
+                break;
+            case INPUT_TYPE.AMOUNT:
+                this.props.Ingredient.Amount = inputValue
+                break;
+        }
     };
 
     render() {
@@ -48,11 +45,11 @@ class MealIngredientForm extends Component {
             <div class="form-group row">
                 <label for="example-text-input" class="col-1 col-form-label">Name</label>
                 <div class="col-3">
-                    <input class="form-control" onBlur={this.inputOnBlur} onChange={this.onDataChange} defaultValue={this.state.Ingredient.Name} type="text" id={INPUT_TYPE.NAME} />
+                    <input class="form-control" onBlur={this.inputOnBlur} onChange={this.onDataChange} defaultValue={this.props.Ingredient.Name} type="text" id={INPUT_TYPE.NAME} />
                 </div>
                 <label for="example-text-input" class="col-1 col-form-label">Amount</label>
                 <div class="col-2">
-                    <input class="form-control" onBlur={this.inputOnBlur} onChange={this.onDataChange} defaultValue={this.state.Ingredient.Amount} type="text" id={INPUT_TYPE.AMOUNT} />
+                    <input class="form-control" onBlur={this.inputOnBlur} onChange={this.onDataChange} defaultValue={this.props.Ingredient.Amount} type="text" id={INPUT_TYPE.AMOUNT} />
                 </div>
                 <div class="col-2" onClick={this.delete}>
                     <i className="fa fas fa-minus-circle fa-2x"></i>
