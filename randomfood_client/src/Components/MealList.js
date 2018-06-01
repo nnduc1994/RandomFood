11a50c914110fb3e0199c4caed5ce7699453b50e
Duc Nguyen <nnduc1994@gmail.com>
2018-06-01 15:49:42 +0300

    Update UI in card list

--- randomfood_client/src/Components/MealList.js
+++ randomfood_client/src/Components/MealList.js
@@ -8,28 +8,15 @@ class MealList extends Component
     constructor(props)
     {
         super(props);
-        this.createCardRows = this.createCardRows.bind(this);
-    }
-
-    createCardRows(meals)
-    {  
-       let mealColumns = meals.map((meal) => {
-            return <div className="col-md-3 d-flex"><MealCard meal={meal}></MealCard></div>
-       });
-
-       return(
-            <div className="meal-card-list row">
-                {mealColumns} 
-            </div>
-       );
     }
 
     render()
     {        
-        let mealChunkArray = _.chunk(this.props.meals, 4);
-        let rows = mealChunkArray.map(x => this.createCardRows(x));
+        let rows = this.props.meals.map(x => { return (<MealCard meal={x}></MealCard>)});
         return(
-            rows
+            <div className="card-columns">
+                {rows}
+            </div>
         )
     }
 }
